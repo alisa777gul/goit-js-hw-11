229930421524a3b12d7b21b9780bad1dd87cdebd
aliso4ka <gulyayevaalisa@gmail.com>
2024-09-15 19:21:07 +0200

    ready

--- src/main.js
+++ src/main.js
@@ -6,19 +6,23 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 
 const formNew = document.querySelector(".formImg");
 const photoList = document.querySelector(".list-img");
-const fetchBtn = document.querySelector(".buttonSearch");
+const loading = document.querySelector(".loader");
+
 const inputSearch = document.getElementById("search-img");
 import { renderUsers } from './js/render-functions.js';
 import { fetchPhotos } from './js/pixabay-api.js';
 
 formNew.addEventListener("submit", (event) => {
     event.preventDefault(); 
+    loading.classList.remove("visually-hidden");
     const query = inputSearch.value.trim();
     if (query !== "") { 
+        
         fetchPhotos(query)
-            .then((photos) => renderUsers(photos, photoList)) // Pass photoList here
+            .then((photos) => { loading.classList.add("visually-hidden"); renderUsers(photos, photoList) }) 
             .catch((error) => console.log(error));
     } else {
+         loading.classList.add("visually-hidden");
         iziToast.error({
             title: 'Error',
             message: 'Sorry, there are no images matching your search query. Please try again!', 
